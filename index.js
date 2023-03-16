0c7f67c4de6c4d881afe85dfd77f633b7212fdae
AkshayV30 <akshayatwork.v30@gmail.com>
2023-03-17 00:02:21 +0530

    update

--- index.js
+++ index.js
@@ -3,7 +3,6 @@ const redis = require("redis");
 const process = require("process");
 
 const app = express();
-
 const client = redis.createClient({
   host: "redis-server",
   port: 6379,
@@ -11,13 +10,13 @@ const client = redis.createClient({
 client.set("visits", 0);
 
 app.get("/", (req, res) => {
-  // process.exit(0);
+  process.exit(0);
   client.get("visits", (err, visits) => {
-    res.send("number of visits" + visits);
+    res.send("Number of visits " + visits);
     client.set("visits", parseInt(visits) + 1);
   });
 });
 
-app.listen(8080, () => {
-  console.log("listening on port 8080");
+app.listen(8081, () => {
+  console.log("listening on port 8081");
 });
